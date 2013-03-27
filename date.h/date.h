cd1a717c5cc56f6186a3b2e0fb9ddd90fddaf947
RashaNabil <rashanabeel1@hotmail.com>
2013-03-28 00:46:20 +0300

    Update date.h

--- date.h/date.h
+++ date.h/date.h
@@ -1,20 +1,37 @@
+
 #ifndef DATE_H
 #define DATE_H
+
+#include <string>
 using namespace std;
-class date{
-  int day;
-	int month;
-	int year;
+
+
+// Data class definition
+class Date
+{
 public:
-	date(int=1 , int= 1 , int = 1990);
-	void setDay(int);
-	void setMonth(int);
-	void setYear(int);
-	int getDay();
-	int getMonth();
-	int getYear();
-	void dat();
-	void printDate();
+	static const int monthsPerYear = 12;
+	Date(int=1 , int= 1 , int = 1990); //default constructor
+
+	// set functions
+	void setDate( int, int, int ); //set Day, Month, Year 
+	void setDay(int); //set Day (after validation)
+	void setMonth(int); //set Month (after validation)
+	void setYear(int); //set Year (after validation)
+
+	// get functions
+	int getDay(); //return Day
+	int getMonth(); //return Moth
+	int getYear(); //return Year
+
+	void printDate();// output Date Information
+
+private:
+	int day; // 1-31 based on month
+	int month; // 1-12
+	int year; // any year
+	int checkDay( int ) const ;// utility function to check if day is proper for month & year
+
 };//end class Date
 
 #endif
