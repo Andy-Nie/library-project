af63ed356ab949a48fcb34c39b5dd7bfbc87a6b6
RashaNabil <rashanabeel1@hotmail.com>
2013-03-27 23:53:55 +0300

    Create media.h

---
+++ media.h/media.h
@@ -0,0 +1,38 @@
+#ifndef MEDIA_H
+#define MEDIA_H
+#include <string>
+#include "Student.h"
+#include "date.h"
+class media
+{
+private:
+  string name;
+	string ISBN;
+	 int NoOfCopies;
+	int CopiesAvailable;
+	student Borrower[2];
+	date BorrowDate[2];
+public:
+	
+	media(string="",string="",int=0,int=2);
+	void setno(int);
+	void setbookname();
+	void setisbn();
+	void setCopiesAvailable();
+	int getno();
+	void getCopiesAvailable();
+	string getbookname();
+	string getisbn();
+	void Borrow(void); // fills the student entry, and BorrowDate
+	//// updates NoOfCopies and increases student datamember 'NoOfBorrowedBooks'
+// only 2 copies of a certain book are allowed to be borrowed
+	void Return(void);
+ // empties the student entry, then shift student array to compact it
+ // and the same for BorrowDate
+//updates NoOfCopies and decreases student datamember 'NoOfBorrowedBooks'
+	
+	void PrintInfo(void);
+// displays all object information and must contain of students who borrowed it with dates
+
+};
+#endif;
